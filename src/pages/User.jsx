cc53418a1d64bb81a70219e626cca9d85f93aa54
devcarlan <devcarlan@gmail.com>
2022-08-03 16:02:22 -0400

    refactoring complete. v1 complete for now

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -11,7 +11,7 @@ import { useParams } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
 import GithubContext from '../context/github/GithubContext'
-import { getUser, getUserRepos } from '../context/github/GitHubActions'
+import { getUserAndRepos } from '../context/github/GitHubActions'
 
 function User() {
   const { user, loading, repos, dispatch } = useContext(GithubContext)
@@ -21,11 +21,8 @@ function User() {
   useEffect(() => {
     dispatch({ type: 'SET_LOADING' })
     const getUserData = async () => {
-      const userData = await getUser(params.login)
-      dispatch({ type: 'GET_USER', payload: userData })
-
-      const userRepoData = await getUserRepos(params.login)
-      dispatch({ type: 'GET_REPOS', payload: userRepoData })
+      const userData = await getUserAndRepos(params.login)
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData })
     }
 
     getUserData()
