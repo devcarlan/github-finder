3a6e3642ae56f32d87367452fedf376bbcfad1c3
devcarlan <devcarlan@gmail.com>
2022-09-30 13:53:58 -0400

    centering user search component

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -27,21 +27,21 @@ function UserSearch() {
   }
 
   return (
-    <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
+    <div className='mx-autp grid grid-cols-1 mb-8 gap-8'>
       <div>
         <form onSubmit={handleSubmit}>
           <div className='form-control'>
-            <div className='relative'>
+            <div className='flex items-center justify-center'>
               <input
                 type='text'
-                className='w-full pr-40 bg-gray-200 input input-lg text-black'
+                className='w-1/2 pr-40 bg-gray-200 input input-lg rounded-r-none text-black'
                 placeholder='Search'
                 value={text}
                 onChange={handleChange}
               />
               <button
                 type='submit'
-                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
+                className='top-0 right-0 rounded-l-none w-36 btn btn-lg'
               >
                 Go
               </button>
